2dffd6ef3153ac40a2ed0766a470e6b86c7fa1bf
Takahiro ISHIDA <i.am.eager.for.peace@gmail.com>
2014-09-05 11:11:39 +0900

    new function to add targetURLs from the web page

---
+++ chiefManager.php
@@ -0,0 +1,15 @@
+<?php
+#targetURLをtargetURLList.csvから読み込んでzeus.phpを叩く
+class chiefManager {
+	
+
+	#viewから受け取ったtargetURL先をtargetURLList.csvに追加していく
+	private function targetURLListGenerator() {
+
+	}
+
+	#targetURLList.csvからtarget先を読み込んで全て配列とする
+	private function loadTargetURL() {
+
+	}
+}
